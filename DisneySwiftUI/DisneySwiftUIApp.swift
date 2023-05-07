366002c4208dc9946c1f737b07cf3ec3c49dd15a
Anna Nosyk <anna.nosyk89@gmail.com>
2023-05-07 14:35:49 +0200

    Initial Commit

---
+++ DisneySwiftUI/DisneySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneySwiftUIApp.swift
+//  DisneySwiftUI
+//
+//  Created by Anna Nosyk on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DisneySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
