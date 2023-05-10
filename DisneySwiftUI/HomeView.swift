4e5d25605bf166bc3ce5498310ed560c632e06e0
Anna Nosyk <anna.nosyk89@gmail.com>
2023-05-10 15:16:59 +0200

    - adding animation effects to the menuView  - adding animation effects to the homeView

--- DisneySwiftUI/HomeView.swift
+++ DisneySwiftUI/HomeView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var showProfile = false
+    @State var viewState = CGSize.zero
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("background")
+                .edgesIgnoringSafeArea(.all)
+            Home(showProfile: $showProfile)
+            .padding(.top, 44)
+            .background(Color.white)
+            .edgesIgnoringSafeArea(.all)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset(y: showProfile ? -450 : 0)
+            .rotation3DEffect(Angle(degrees: Double(showProfile ? (viewState.height/10) - 10 : 0)), axis: (x: 10, y: 0, z: 0))
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            .edgesIgnoringSafeArea(.all)
+            MenuView()
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ?  0 : screen.height)
+                .offset(y: viewState.height)
+                .animation(.spring(response: 0.7, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(DragGesture()
+                    .onChanged({ value in
+                        self.viewState = value.translation
+                        if self.viewState.height > 50 {
+                            self.showProfile = false
+                        }
+                    })
+                        .onEnded({ value in
+                            self.viewState = .zero
+                        })
+                )
+        }
+        
     }
 }
 
@@ -18,3 +54,20 @@ struct HomeView_Previews: PreviewProvider {
         HomeView()
     }
 }
+
+struct AvatarView: View {
+    @Binding var showProfile:Bool
+    var body: some View {
+        Button {
+            self.showProfile.toggle()
+        } label: {
+            Image("human4")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
